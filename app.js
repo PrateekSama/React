66b7df678100981ef7b22e0928972c41aa65f5ee
Prateek Prakhar <prateekprakhar@Prateeks-MacBook-Air.local>
2024-04-13 17:58:52 +0530

    Episode 1

---
+++ app.js
@@ -0,0 +1,19 @@
+const parent = React.createElement(
+    "div",
+    {id:"parent"},
+   [ React.createElement(
+    "div",
+    {id:"child"},
+[React.createElement("h1",{},"i am H1 tag"),
+React.createElement("h2",{},"I am h2")]
+), React.createElement(
+    "div",
+    {id:"child2"},
+[React.createElement("h1",{},"i am H1 tag"),
+React.createElement("h2",{},"I am h2")]
+)]
+);
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
